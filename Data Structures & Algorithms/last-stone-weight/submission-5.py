672ae71fcc31f7c0a1b0bbc222557bf9ccb5cab9
Dongeun Kim <74619981+dkim1112@users.noreply.github.com>
2026-06-19 09:16:17 +0900

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # array of int "stones", stones[i] = weight of ith stone
+        
+        # each step, choose 2 heaviest stones, with weight x, y
+        # smash two together
+        # if x == y, both destroyed
+        # if x < y, w destroyed & y weight = y - x
+
+        # continue until <= 1 stone left
+        # return weight of last stone or 0 if none
+
+        # via max-heap (heapify), we keep the biggest at top.
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1: # not if (because continuosly removed & added)
+            x = heapq.heappop(stones)
+            y = heapq.heappop(stones)
+            if x < y:
+                diff = x - y # remember, they're stored in negative int!
+                heapq.heappush(stones, diff) # adding back in new y (with new weight)
+            
+        stones.append(0)
+        return abs(stones[0])
